8a319a10a91340e884d7b7cc04c8f36cdad7a944
bryanhayslip <bryan.hayslip@gmail.com>
2022-05-17 15:03:57 +0000

    created GET methods

--- src/main.py
+++ src/main.py
@@ -30,16 +30,43 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def get_people():
+     json_text = jsonify(people)
+     return json_text
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people_id():
+     json_text = jsonify(people.results.id)
+     return json_text
 
-    return jsonify(response_body), 200
+@app.route('/people', methods=['POST'])
+def add_people():
+    payload = request.get_json(force=True)
+    people.append(payload)
+    return jsonify(people)
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+     json_text = jsonify(planets)
+     return json_text
+
+@app.route('/planets', methods=['POST'])
+def add_planets():
+    payload = request.get_json(force=True)
+    planets.append(payload)
+    return jsonify(planets)
+
+@app.route('/users', methods=['GET'])
+def get_users():
+     json_text = jsonify(users)
+     return json_text
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+planets = [{ "label": "My first planet", "done": False}]
+people = [{"results": [{ "name": "Luke Skywalker", "id": 1, "height": "172", "mass": "77", "done": False }]}]
+users = [{ "label": "My first user", "done": False}]
